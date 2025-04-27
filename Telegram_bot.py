52042386907d36c68ba2dd019ae1d858d9a60482
Athiel Gutierrez <168398323+AthielGalesh@users.noreply.github.com>
2025-04-27 01:28:28 -0600

    Update Telegram_bot.py

--- Telegram_bot.py
+++ Telegram_bot.py
@@ -1,28 +1,22 @@
 import os, time, requests, psutil
-from dotenv import load_dotenv
+
+from globals import BOT_TOKEN, CHAT_ID
 
 
+def send_message(message):
+    bot_path = BOT_TOKEN  #USE YOUR CUSTOM CREDENTIALS, IMPORT THEM FROM GLOBALS
+    chat_url = CHAT_ID
+    telegram_url = "https://api.telegram.org/bot"
+    text_url = f'&text={message}'
+    final_url = telegram_url + bot_path + chat_url + text_url
+    response = requests.get(final_url)
+    assert response.status_code == 200, f"Error: {response.status_code}"
 
+#testing telegram bot
 
-#CREDENTIALS
-BOT_TOKEN = Telegram_bot_token #Place your telegram values here
-CHAT_ID = Chat_id_value
+send_message("Hello Telegram from Python!")
 
-
-
-
-class Telegram_Mensaje:
-    def send_telegram(self, message="test"):
-        telegram_url = "https://api.telegram.org"
-        bot_path = f"/bot{BOT_TOKEN}"
-        chat_url = f"/sendMessage?chat_id={CHAT_ID}"
-        text_url = f"&text={message}"
-        final_url = telegram_url + bot_path + chat_url + text_url
-        response = requests.get(final_url)
-        assert response.status_code == 200
-
-#you can also check your device if you are running your own server, for example:
 while True:
     time.sleep(5)
     msg = "the cpu usage is {}%".format(psutil.cpu_percent(interval=4))
-    send_telegram(msg)
+    send_message(msg)
